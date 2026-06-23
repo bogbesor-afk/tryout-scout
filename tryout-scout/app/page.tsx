7581b75b9cedbc88dd2e2033425f7e0eb1a54ad8
bogbesor-afk <bogbesor@gmail.com>
2026-06-23 15:01:24 -0400

    Move title higher into the grass area of the background

--- tryout-scout/app/page.tsx
+++ tryout-scout/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         {/* Dark overlay */}
         <div className="absolute inset-0 bg-black/55 pointer-events-none" />
 
-        <div className="relative flex flex-col items-center text-center gap-4 -mt-8">
+        <div className="relative flex flex-col items-center text-center gap-4 -mt-32">
           <h1 className="text-6xl font-black text-white tracking-widest uppercase drop-shadow-xl">
             Tryout Scout
           </h1>
